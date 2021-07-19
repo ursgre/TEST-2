a98f026016596c241726aa56107247368232adbc
ursgre <urszulagrezicka@gmail.com>
2021-07-19 15:43:50 +0200

    Initial commit

---
+++ kalkulator.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Oblicz objętość kuli</h2>
+
+<p id="demo"></p>
+
+<script>
+window.alert("Udane logowanie!");
+</script>
+
+<script>
+    var R = parseFloat (prompt ("Podaj promień kuli : "));
+    document.write ((4/3)*3,14*R*R*R+<br>);
+</script>
+
+</body>
+</html>
